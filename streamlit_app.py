ce378b1c594ddc296e397b12db2d9962c79cbc12
Akkaash Goel <akkaash.goel@snowflake.com>
2023-06-29 13:52:30 -0700

    day-9

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,11 @@ start_time = st.slider(
      value=datetime(2020, 1, 1, 9, 30),
      format="MM/DD/YY - hh:mm")
 st.write("Start time:", start_time)
+
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
